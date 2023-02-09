e00ab146f8d7641d0cafa8006c2cbde036073df3
Pedro Silva <pedro@andoras.com.br>
2023-02-09 12:06:45 -0300

    feat(controllers): controllers class
    create cotrollers class to process data from request and response

---
+++ src/controllers.ts
@@ -0,0 +1,36 @@
+import { User, Product } from './index';
+import { Request, Response } from 'express';
+import { Models } from './models';
+
+export class Controllers {
+  async listUsers(req: Request, res: Response) {
+    const Model = new Models();
+
+    const users: User[] = await Model.listUsers();
+
+    return res.json(users);
+  }
+
+  async listProducts(req: Request, res: Response) {
+    const Model = new Models();
+
+    const products: Product[] = await Model.listProducts();
+
+    return res.json(products);
+  }
+
+  async getBudgetValue(req: Request, res: Response) {
+    const { userid } = req.params;
+
+    const { productsIds } = req.body;
+
+    const Model = new Models();
+
+    const value = await Model.getBudgetValue(
+      userid as unknown as number,
+      productsIds
+    );
+
+    return res.json({ value });
+  }
+}
