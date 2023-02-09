7c9b757b97d85167fdf33369c880f64fc6bc0480
Pedro Silva <pedro@andoras.com.br>
2023-02-09 10:51:49 -0300

    feat(api): routing with express Router
    created route to get users list, products list and budget value.

---
+++ src/router.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+export const RouterInstance = Router();
+
+RouterInstance.get('/users');
+
+RouterInstance.get('/products');
+
+RouterInstance.post('/value');
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import { RouterInstance } from './router';
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => res.send('olá mundo'));
+server.use(RouterInstance);
 
 server.listen(process.env.PORT || 9001);
